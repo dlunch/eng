fe638661ff54c809af3d771542a5b25df695dd41
Inseok Lee <git@dlun.ch>
2021-10-11 18:31:49 +0900

    typo

--- src/renderer.rs
+++ src/renderer.rs
@@ -27,7 +27,7 @@ pub struct Renderer {
 
 impl Renderer {
     pub async fn new<W: HasRawWindowHandle>(window: &W, width: u32, height: u32) -> Self {
-        let instance = wgpu::Instance::new(wgpu::Backends::All);
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
         let surface = unsafe { instance.create_surface(window) };
 
         let adapter = instance
