27ed60e57f7639e84aca5c33a2973001e6459648
Inseok Lee <git@dlun.ch>
2022-05-29 17:12:28 +0900

    Do not store size and align

--- src/ecs/raw_vec.rs
+++ src/ecs/raw_vec.rs
@@ -1,73 +1,70 @@
 use alloc::vec::Vec;
-use core::{any::TypeId, slice};
+use core::{
+    any::TypeId,
+    mem::{align_of, size_of},
+    slice,
+};
 
 // homogeneous vec
 pub struct RawVec {
     storage: Vec<u8>,
     actual_type: TypeId,
-    size: usize,
-    item_size: usize,
 }
 
 impl RawVec {
     pub fn new<T: 'static>() -> Self {
-        let actual_type = TypeId::of::<T>();
-        let align = core::mem::align_of::<T>();
-        let size = core::mem::size_of::<T>();
-
-        let item_size = Self::round_up(size, align);
-
         Self {
             storage: Vec::new(),
-            actual_type,
-            size,
-            item_size,
+            actual_type: TypeId::of::<T>(),
         }
     }
 
     pub fn insert<T: 'static>(&mut self, index: usize, value: T) {
-        let offset = index * self.item_size;
+        let item_size = Self::round_up(size_of::<T>(), align_of::<T>());
+        let offset = index * item_size;
 
         self.insert_at(offset, value);
     }
 
     pub fn get<T: 'static>(&self, index: usize) -> Option<&T> {
-        debug_assert!(TypeId::of::<T>() == self.actual_type);
+        assert!(TypeId::of::<T>() == self.actual_type);
 
-        let offset = index * self.item_size;
+        let item_size = Self::round_up(size_of::<T>(), align_of::<T>());
+        let offset = index * item_size;
+
         if offset >= self.storage.len() {
             return None;
         }
 
-        let value_ptr = &self.storage[offset..offset + self.size] as *const [u8] as *const T;
+        let value_ptr = &self.storage[offset..offset + size_of::<T>()] as *const [u8] as *const T;
         Some(unsafe { &*value_ptr })
     }
 
     pub fn get_mut<T: 'static>(&mut self, index: usize) -> Option<&mut T> {
-        debug_assert!(TypeId::of::<T>() == self.actual_type);
+        assert!(TypeId::of::<T>() == self.actual_type);
 
-        let offset = index * self.item_size;
+        let item_size = Self::round_up(size_of::<T>(), align_of::<T>());
+        let offset = index * item_size;
         if offset >= self.storage.len() {
             return None;
         }
 
-        let value_ptr = &mut self.storage[offset..offset + self.size] as *mut [u8] as *mut T;
+        let value_ptr = &mut self.storage[offset..offset + size_of::<T>()] as *mut [u8] as *mut T;
         Some(unsafe { &mut *value_ptr })
     }
 
     pub fn iter<T: 'static>(&self) -> impl Iterator<Item = &T> {
-        debug_assert!(TypeId::of::<T>() == self.actual_type);
+        assert!(TypeId::of::<T>() == self.actual_type);
 
-        self.storage
-            .chunks(self.item_size)
-            .map(move |x| unsafe { &*(x as *const [u8] as *const T) })
+        let item_size = Self::round_up(size_of::<T>(), align_of::<T>());
+        self.storage.chunks(item_size).map(move |x| unsafe { &*(x as *const [u8] as *const T) })
     }
 
     fn insert_at<T: 'static>(&mut self, offset: usize, value: T) {
-        debug_assert!(TypeId::of::<T>() == self.actual_type);
+        assert!(TypeId::of::<T>() == self.actual_type);
 
         let value_ptr = &value as *const T as *const u8;
-        let value_slice = unsafe { slice::from_raw_parts(value_ptr, self.size) };
+        let value_slice = unsafe { slice::from_raw_parts(value_ptr, size_of::<T>()) };
         self.storage.splice(offset..offset, value_slice.iter().cloned());
 
         core::mem::forget(value);
