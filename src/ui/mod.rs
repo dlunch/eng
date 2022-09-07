42d2d4a6ec45da1f471cf7912a82a29fe940d698
Inseok Lee <git@dlun.ch>
2022-09-07 21:14:44 +0900

    Use transform for 2d

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,5 +1,7 @@
 use alloc::{sync::Arc, vec};
 
+use glam::Vec3;
+
 use super::{
     ecs::ComponentBundle,
     render::{Material, Mesh, RenderComponent, Renderer, Shader, SimpleVertex, Transform},
@@ -15,13 +17,10 @@ pub struct Rectangle {
 impl ComponentBundle for Rectangle {
     fn add_components(self, world: &mut crate::ecs::World, entity: crate::ecs::Entity) {
         let vertices = vec![
-            SimpleVertex::new([self.x as f32, self.y as f32, 0.0, 1.0], [0.0, 0.0]),
-            SimpleVertex::new([self.x as f32, self.y as f32 + self.height as f32, 0.0, 1.0], [1.0, 0.0]),
-            SimpleVertex::new([self.x as f32 + self.width as f32, self.y as f32, 0.0, 1.0], [0.0, 1.0]),
-            SimpleVertex::new(
-                [self.x as f32 + self.width as f32, self.y as f32 + self.height as f32, 0.0, 1.0],
-                [0.0, 1.0],
-            ),
+            SimpleVertex::new([0.0, 0.0, 0.0, 1.0], [0.0, 0.0]),
+            SimpleVertex::new([0.0, 1.0, 0.0, 1.0], [1.0, 0.0]),
+            SimpleVertex::new([1.0, 0.0, 0.0, 1.0], [0.0, 1.0]),
+            SimpleVertex::new([1.0, 1.0, 0.0, 1.0], [1.0, 1.0]),
         ];
 
         let indices = vec![0, 1, 2, 2, 1, 3];
@@ -32,7 +31,13 @@ impl ComponentBundle for Rectangle {
         let shader = Shader::new(renderer, include_str!("ui.wgsl"));
         let material = Material::with_custom_shader(renderer, &[], Arc::new(shader));
 
-        let component = RenderComponent::new(mesh, material, Transform::new());
+        let transform = Transform::with_values(
+            Vec3::new(self.x as f32, self.y as f32, 0.0),
+            Vec3::new(0.0, 0.0, 0.0),
+            Vec3::new(self.width as f32, self.height as f32, 0.0),
+        );
+
+        let component = RenderComponent::new(mesh, material, transform);
 
         world.add_component(entity, component)
     }
