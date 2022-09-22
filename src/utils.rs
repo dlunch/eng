33912f20eeaaaf208c056f0ea359f348ab8a3e6a
Inseok Lee <git@dlun.ch>
2022-09-22 16:56:32 +0900

    Move round_up to utils

--- src/utils.rs
+++ src/utils.rs
@@ -15,3 +15,18 @@ where
         num_to_round + multiple - remainder
     }
 }
+
+//returns zero if v is zero.
+pub fn round_up_power_of_two(mut v: u32) -> u32 {
+    //from http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2 (public domain)
+
+    v -= 1;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v += 1;
+
+    v
+}
