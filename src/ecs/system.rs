68b34732b96bedf7f7b81e05ed3d46abf6bdcf7a
Inseok Lee <git@dlun.ch>
2022-12-24 17:35:14 +0900

    Add 2 argument version system

--- src/ecs/system.rs
+++ src/ecs/system.rs
@@ -16,25 +16,31 @@ pub trait System {
     fn run(&self, world: &World) -> CommandList;
 }
 
-pub struct SystemFunction<F, Input>(F, PhantomData<Input>)
-where
-    Input: SystemInput;
+pub struct SystemFunction<F, Input>(F, PhantomData<Input>);
 
-impl<F, Input> SystemFunction<F, Input>
-where
-    Input: SystemInput,
-{
+impl<F, Input> SystemFunction<F, Input> {
     pub fn new(f: F) -> Self {
-        SystemFunction(f, PhantomData)
+        Self(f, PhantomData)
     }
 }
 
-impl<T, Input> System for SystemFunction<T, Input>
+impl<T, Input1> System for SystemFunction<T, (Input1,)>
 where
-    T: Fn(&Input) -> CommandList,
-    Input: SystemInput,
+    T: Fn(&Input1) -> CommandList,
+    Input1: SystemInput,
 {
     fn run(&self, world: &World) -> CommandList {
-        (self.0)(Input::from_world(world))
+        (self.0)(Input1::from_world(world))
+    }
+}
+
+impl<T, Input1, Input2> System for SystemFunction<T, (Input1, Input2)>
+where
+    T: Fn(&Input1, &Input2) -> CommandList,
+    Input1: SystemInput,
+    Input2: SystemInput,
+{
+    fn run(&self, world: &World) -> CommandList {
+        (self.0)(Input1::from_world(world), Input2::from_world(world))
     }
 }
